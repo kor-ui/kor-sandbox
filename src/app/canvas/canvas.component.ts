5978016f3e295d04bbf5642a15278a21ec0d61c0
Eduardo Ferreira <eduardo.fernandes_ferreira@siemens-healthineers.com>
2020-05-03 14:24:05 +0200

    [done] scale and viewport

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -8,7 +8,44 @@ import { ComponentsService } from '../services/components.service';
   styleUrls: ['./canvas.component.scss'],
 })
 export class CanvasComponent implements OnInit {
+
+  public viewports: any[] = [
+    {
+      name: "desktop",
+      icon: "desktop_windows",
+      height: "1080px",
+      width: "1920px"
+    },
+    {
+      name: "phone_vertical",
+      icon: "smartphone",
+      height: "667px",
+      width: "375px"
+    },
+    {
+      name: "tablet_vertical",
+      icon: "tablet_mac",
+      height: "1024px",
+      width: "768px"
+    },
+    {
+      name: "tablet_horizontal",
+      icon: "tablet",
+      height: "768px",
+      width: "1024px"
+    }
+  ];
+  public currentViewport: any = {
+    height: this.viewports[0]?.height,
+    width: this.viewports[0]?.width
+  };
+  public currentScale: number = 0.5;
+
   constructor(public drag: DragService, public components: ComponentsService) {}
 
   ngOnInit(): void {}
+
+  public round(num: number): number {
+    return Math.round(num);
+  }
 }
