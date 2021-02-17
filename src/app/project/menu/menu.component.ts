6172a4a916639c0eaa335718157e4493d468a391
Eduardo Ferreira <eduardo.fernandes_ferreira@siemens-healthineers.com>
2021-02-17 16:46:30 +0100

    [update] firebase deploy

--- src/app/project/menu/menu.component.ts
+++ src/app/project/menu/menu.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { DragService } from '../../services/drag.service';
-import { ComponentsService } from '../../services/components.service';
+import { Component, OnInit } from "@angular/core";
+import { DragService } from "../../services/drag.service";
+import { ComponentsService } from "../../services/components.service";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss'],
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.scss"],
 })
 export class MenuComponent implements OnInit {
   constructor(public drag: DragService, public components: ComponentsService) {}
 
-  ngOnInit(): void {
-    console.log(this.components.allComponents);
-  }
+  ngOnInit(): void {}
 }
