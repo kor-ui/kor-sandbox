d3cebdc7af6f186521862ee6935dfc32ad2199ad
Eduardo Ferreira <eduardo.fernandes_ferreira@siemens-healthineers.com>
2021-02-19 17:28:55 +0100

    [fix] production rollback

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ProjectsComponent } from './projects/projects.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'projects',
+    redirectTo: 'project',
     pathMatch: 'full'
   },
   {
